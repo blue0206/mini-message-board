532dc67d331be3836b197002f6c15df17da7c72f
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-05 19:29:40 +0530

    Add initial messages data structure to db.ts

---
+++ src/db.ts
@@ -0,0 +1,16 @@
+import { message } from "./types/index.type.js";
+
+const messages: message[] = [
+    {
+        text: "Hi there!",
+        user: "Blue",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Aayush",
+        added: new Date()
+    }
+];
+
+export { messages };
