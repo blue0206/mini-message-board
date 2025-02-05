2af4d8ea4e66162dfa5a1dde4de279de570f7274
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-05 19:31:04 +0530

    Implement new message submission route with validation and rendering

--- src/routers/new.router.ts
+++ src/routers/new.router.ts
@@ -1,9 +1,30 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { messages } from "../db.js";
+import { message } from "../types/index.type.js";
 
 const newRouter = Router();
 
-newRouter.get('/', (req, res) => {
-    res.status(200).send("New Message");
+newRouter.get('/', (req: Request, res: Response) => {
+    res.status(200).render("form");
+});
+
+newRouter.post('/', (req: Request, res: Response) => {
+    const authorName: string = req.body?.authorName;
+    const message: string = req.body?.message;
+
+    if (!authorName.trim() || !message.trim()) {
+        return res.status(401).redirect("/new");
+    }
+
+    const messageInstance: message = {
+        text: message,
+        user: authorName,
+        added: new Date()
+    };
+
+    messages.push(messageInstance);
+
+    res.status(200).redirect("/");
 });
 
 export default newRouter;
