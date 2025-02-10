f4a3389fba5beba298f382e0bdcd1e0cb7e54cc7
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-11 00:38:25 +0530

    Refactor timestamp utility functions to simplify date conversion

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -1,21 +1,14 @@
 import { format } from 'date-fns';
 
-function convertUTCDateToLocalDate(date: Date) {
-    var newDate = new Date(date);
-    newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-
-    return newDate;   
-}
-
-const getMessageTimestamp = (dbTimestamp: Date): string => {
-    const localDate = convertUTCDateToLocalDate(new Date(dbTimestamp));
+const getMessageTimestamp = (dbTimestamp: any): string => {
+    const localDate = new Date(dbTimestamp);
     const date = format(localDate.toLocaleString(), "h:mm a");
 
     return date;
 }
 
 const getMessageDate = (dbDate: Date): string => {
-    const localDate = convertUTCDateToLocalDate(new Date(dbDate));
+    const localDate = new Date(dbDate);
     const date = format(localDate.toLocaleString(), "MMMM do, yyyy");
 
     return date;
