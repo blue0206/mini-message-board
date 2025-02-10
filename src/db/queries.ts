b7cda73edd4c0d4c40eac6b8c032f2432b03d534
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-10 15:58:21 +0530

    Add functions to retrieve and create messages with time formatting

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,12 +1,32 @@
 import pool from "./pool.js";
 import { QueryResultRow } from "pg";
+import { getMessageTimestamp, getMessageDate } from "../utils/timestamp.js";
 
 async function getAllMessages() {
     const result = await pool.query("SELECT * FROM messages");
-    const rows: QueryResultRow[] = result.rows;
+    const rows = result.rows.map((row: QueryResultRow) => {
+        row.added = getMessageTimestamp(row.added);
+        row.date = getMessageDate(row.date);
+        return row;
+    });
+
     return rows;
 }
 
+async function getMessage(id: number) {
+    const result = await pool.query("SELECT * FROM messages WHERE id = $1", [id]);
+    const row: QueryResultRow = result.rows[0];
+    row.added = getMessageTimestamp(row.added);
+    row.date = getMessageDate(row.date);
+    return row;
+}
+
+async function createMessage(text: string, user: string) {
+    await pool.query('INSERT INTO messages (text, "user") VALUES ($1, $2)', [text, user]);
+}
+
 export {
-    getAllMessages
+    getAllMessages,
+    getMessage,
+    createMessage
 }
