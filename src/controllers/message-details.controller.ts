ef6685517aaff005e1038e6653e96cd4079e7c97
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-10 16:33:17 +0530

    Refactor error handling in getMessageDetails to simplify not found message

--- src/controllers/message-details.controller.ts
+++ src/controllers/message-details.controller.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from 'express';
 import asyncHandler from 'express-async-handler';
 import { getMessage } from '../db/queries.js';
-// import { messages } from '../db.js';
 import CustomNotFoundError from '../errors/CustomNotFoundError.js';
 
 const getMessageDetails = asyncHandler(async (req: Request, res: Response) => {
     const messageID = req.params.messageID;
     const message = await getMessage(Number(messageID));
     if (!message) {
-        throw new CustomNotFoundError(`Message with ID ${messageID} not found.`);
+        throw new CustomNotFoundError(`Message not found.`);
     }
     res.status(200).render("message-details", { message });
 });
