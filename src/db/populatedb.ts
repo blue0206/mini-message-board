6e5df97b5019d76441b0490cb14738f50ed24097
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-10 13:06:35 +0530

    Refactor database seeding script
    - Use environment variables for connection details
    - Fix column name for user in messages table

--- src/db/populatedb.ts
+++ src/db/populatedb.ts
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+import "dotenv/config";
 import pg from "pg";
 
 const { Client } = pg;
@@ -8,12 +9,12 @@ const SQL = `
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     text TEXT NOT NULL,
-    user VARCHAR(200) NOT NULL,
+    "user" VARCHAR(200) NOT NULL,
     added TIMESTAMP NOT NULL DEFAULT NOW(),
     date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-INSERT INTO messages (text, user)
+INSERT INTO messages(text, "user")
 VALUES
     ('Hi there!', 'Blue'),
     ('Hello World!', 'Aayush');
@@ -22,11 +23,26 @@ VALUES
 async function main() {
     console.log("Seeding...");
     const client = new Client({
-        connectionString: process.env.DATABASE_CONNECTION_URI
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_DATABASE,
+        ssl: {
+            rejectUnauthorized: true,
+            ca: process.env.DB_CA,
+        }
     });
-    await client.connect();
-    await client.query(SQL);
-    await client.end();
+    client.connect((err) => {
+        if (err) throw err;
+        client.query(SQL, (err, result) => {
+            if (err) throw err;
+            console.log(result.rows);
+            client.end((err) => {
+                if (err) throw err;
+            });
+        });
+    });
     console.log("Done!");
 }
 
