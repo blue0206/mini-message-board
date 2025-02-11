08a6919799a846553e59fe56a7dc1a61c66d1426
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-11 17:33:11 +0530

    Remove debug logging from timestamp utility function

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -4,7 +4,6 @@ import moment from 'moment-timezone';
 const getMessageTimestamp = (time: string, timeZone: string | undefined): string => {
     const utcDate = new Date(time);
     const timeArr = time.split(" ").filter((part) => part.length > 0 && part !== '\n');
-    console.log("Time Array:\t", timeArr);
     const year = parseInt(timeArr[3]);
 
     // Use a map to convert month names to numbers
@@ -20,19 +19,12 @@ const getMessageTimestamp = (time: string, timeZone: string | undefined): string
     const minute = parseInt(hourArr[1]);
     const second = parseInt(hourArr[2]);
 
-    // Log each component to ensure they are correct
-    console.log("Year:", year, "Month:", month, "Day:", day, "Hour:", hour, "Minute:", minute, "Second:", second);
-
     const customDate = new Date(Date.UTC(year, month, day, hour, minute, second));
-    console.log("CUSTOM DATE\t", customDate);
-
-    console.log("UTC | Time\t", utcDate, time);
 
     if (timeZone) {
-
         const localDate = moment.utc(customDate).tz(timeZone).format("YYYY-MM-DD HH:mm:ss");
-        console.log("LOCAL DATE\t", localDate);
         const date = format(localDate, "h:mm a");
+        
         return date;
     }
     return format(utcDate.toLocaleString(), "h:mm a");
