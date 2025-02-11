6e1b4ee4dbcb7c6966224047d3b595ed6d1b575d
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-11 17:53:33 +0530

    Refactor import statements; update comments for clarity

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import "dotenv/config";
 import path from 'path';
 import indexRouter from './routers/index.router.js';
 import newRouter from './routers/new.router.js';
 import messageRouter from './routers/message-details.router.js';
-import timeFormatRouter from './routers/date-time-format.router.js';
+import timeFormatRouter from './routers/time-format.router.js';
 import CustomNotFoundError from './errors/CustomNotFoundError.js';
 import InvalidInputError from './errors/InvalidInputError.js';
 
 const app = express();
 
-// EJS Middlewares
+// Middlewares
 app.use(express.static(path.join(import.meta.dirname, "public")));
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
@@ -24,7 +24,7 @@ app.use('/messageDetails', messageRouter);
 app.use('/api', timeFormatRouter);
 
 // Error Middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response) => {
     console.log(err);
     if (err instanceof CustomNotFoundError) {
         res.status(err.statusCode).render("message-details", {
