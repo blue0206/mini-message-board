88fdbc52771703d20a20101e5a35abc9a13074dd
Aayush Rai <105283853+blue0206@users.noreply.github.com>
2025-02-11 13:08:20 +0530

    Use moment-timezone for accurate local date formatting

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -1,7 +1,16 @@
 import { format } from 'date-fns';
+import moment from 'moment-timezone';
 
 const getMessageTimestamp = (dbTimestamp: any): string => {
-    const localDate = new Date(dbTimestamp);
+    const utcDate = new Date(dbTimestamp);
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log("DBTS", dbTimestamp);
+    console.log("UTS", utcDate);
+    const localDate = moment
+      .utc(dbTimestamp)
+      .tz(timeZone)
+      .format("YYYY-MM-DD HH:mm:ss");
+    console.log('localDate', localDate);
     const date = format(localDate.toLocaleString(), "h:mm a");
 
     return date;
